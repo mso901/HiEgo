c5f064d5d974262286753d3197a8e53a308b8fcb
seonokmin <dnsmsdkdl@gmail.com>
2023-12-16 12:14:54 +0900

    change color and before delete divide

--- script/index.js
+++ script/index.js
@@ -8,38 +8,7 @@ $(function () {
      $(".gnb>nav").removeClass("on");
   });
 
-
-  const slider = $(".b_list");
-  const slickOptions = {
-    infinite: true,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    responsive: [
-      {
-        breakpoint: 1280,
-        settings: {
-          slidesToShow: 3,
-        },
-      },
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-        },
-      },
-    ],
-  };
-
-  $(window).on("load resize", function () {
-    if ($(window).width() < 767) {
-      slider.slick("unslick");
-    } else {
-      slider.not(".slick-initialized").slick(slickOptions);
-    }
-  });
-
+//slide
   let Width = 768;
   let screenWidth = $(window).width();
   if (screenWidth < Width) {
@@ -48,6 +17,26 @@ $(function () {
       slidesToShow: 2,
       slidesToScroll: 1,
     });
-  }
+  } else { 
+$('.b_list').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1400,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+    ],
+  });
+  };
 });
 
