915a608a26780080f64870b23aa776bebdc6685e
minsunok <dnsmsdkdl@gmail.com>
2023-12-26 17:21:55 +0900

    change nav

--- script/index.js
+++ script/index.js
@@ -2,32 +2,54 @@
 
 $(function () {
   //주메뉴 슬라이드
+  // let Width = 800;
+  // let screenWidth = $(window).width();
+
+//   $("nav>ul>li").on({
+//     "mouseenter focus": function () {
+//       if (screenWidth < Width) {
+//         $(this).find(".submenu").stop().slideDown();
+//       } else {
+//         $(this).find(".submenu").stop().fadeIn(400);
+//       }
+//     },
+//     "mouseleave blur": function () {
+//       if (screenWidth < Width) {
+//         $(this).find(".submenu").stop().slideUp();
+//       } else {
+//         $(this).find(".submenu").stop().fadeOut(400);
+//       }
+//     },
+//  });
   let Width = 800;
   let screenWidth = $(window).width();
   if (screenWidth < Width) {
-    
-    $('nav>ul>li').mouseenter(function () {
-      $(this).find('.submenu').stop().slideDown();
-    });
-    $('nav>ul>li').mouseleave(function () {
-      $(this).find('.submenu').stop().slideUp();
+    $("nav>ul>li").on({
+      "mouseenter focus": function () {
+        $(this).find(".submenu").stop().slideDown();
+      },
+      "mouseleave blur": function () {
+        $(this).find(".submenu").stop().slideUp();
+      },
     });
   } else {
-    $('nav>ul>li').mouseenter(function () {
-      $('.submenu').stop().fadeIn(400);
-    });
-    $('nav>ul>li').mouseleave(function () {
-      $('.submenu').stop().fadeOut(400);
+    $("nav>ul>li").on({
+      "mouseenter focus": function () {
+        $(".submenu").stop().fadeIn(400);
+      },
+      "mouseleave blur": function () {
+        $(".submenu").stop().fadeOut(400);
+      },
     });
   }
 
   //햄버거 메뉴 클릭
-  $('.gnb>button').click(function (e) {
+  $(".gnb>button").click(function (e) {
     e.preventDefault();
-    $('.gnb>nav').toggleClass('on');
+    $(".gnb>nav").toggleClass("on");
   });
-  $('nav>button').click(function (e) {
+  $("nav>button").click(function (e) {
     e.preventDefault();
-    $('.gnb>nav').removeClass('on');
+    $(".gnb>nav").removeClass("on");
   });
 });
