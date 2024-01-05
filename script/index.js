636e0107dfbcfce9342477b50ae8dc8da34bf824
minsunok <dnsmsdkdl@gmail.com>
2024-01-05 11:44:17 +0900

    submenu fix

--- script/index.js
+++ script/index.js
@@ -2,47 +2,27 @@
 
 $(function () {
   //주메뉴 슬라이드
-  // let Width = 800;
-  // let screenWidth = $(window).width();
-
-//   $("nav>ul>li").on({
-//     "mouseenter focus": function () {
-//       if (screenWidth < Width) {
-//         $(this).find(".submenu").stop().slideDown();
-//       } else {
-//         $(this).find(".submenu").stop().fadeIn(400);
-//       }
-//     },
-//     "mouseleave blur": function () {
-//       if (screenWidth < Width) {
-//         $(this).find(".submenu").stop().slideUp();
-//       } else {
-//         $(this).find(".submenu").stop().fadeOut(400);
-//       }
-//     },
-//  });
   let Width = 800;
-  let screenWidth = $(window).width();
-  if (screenWidth < Width) {
-    $("nav>ul>li").on({
-      "mouseenter focus": function () {
+  $("nav>ul>li").on({
+    "mouseenter keyup": function () {
+        let screenWidth = $(window).width();
+      if (screenWidth < Width) {
         $(this).find(".submenu").stop().slideDown();
-      },
-      "mouseleave blur": function () {
-        $(this).find(".submenu").stop().slideUp();
-      },
-    });
-  } else {
-    $("nav>ul>li").on({
-      "mouseenter focus": function () {
+      } else {
         $(".submenu").stop().fadeIn(400);
-      },
-      "mouseleave blur": function () {
+      }
+    },
+    "mouseleave keydown": function () {
+        let screenWidth = $(window).width();
+      if (screenWidth < Width) {
+        $(this).find(".submenu").stop().slideUp();
+      } else {
         $(".submenu").stop().fadeOut(400);
-      },
-    });
-  }
+      }
+    },
+  });
 
+  
   //햄버거 메뉴 클릭
   $(".gnb>button").click(function (e) {
     e.preventDefault();
